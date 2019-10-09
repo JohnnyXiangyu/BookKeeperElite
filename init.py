e285b33d979ca07c26068a38cd3fe99d60ab6ab3
JohnnyW <JohnnyXiangyu@users.noreply.github.com>
2019-10-08 17:06:24 -0700

    some more

---
+++ init.py
@@ -0,0 +1,24 @@
+import web
+import sqlite3
+
+# TODO: implement server part
+# render = web.template.render('templates/')
+
+# urls = (
+#     '/(.*)', 'index'
+# )
+
+# class index:
+#     def GET(self, name):
+#         return render.home(name)
+
+
+
+# if __name__ == "__main__":
+#     app = web.application(urls, globals())
+#     app.run()
+
+# TODO: implement database
+
+
+print('testpass')
